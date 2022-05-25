305c7b53e31cd37807474b9d88e889b3a9a289b3
Vladimir Likhachev <lettervl@ya.ru>
2022-05-25 21:45:39 +0300

    4. Категории в TODO List [#331991]

--- db/scripts/update_002.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists item_categories (
-    id serial primary key,
-    name text,
-	itemsId BIGINT,
-	FOREIGN KEY (itemsId) REFERENCES item (Id) ON DELETE CASCADE,
-	categoriesId BIGINT,
-	FOREIGN KEY (categoriesId) REFERENCES categories (Id)
-	);
